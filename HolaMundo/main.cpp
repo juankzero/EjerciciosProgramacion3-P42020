f00c96cb068e08bd589c4022d3971359f6781759
juankzero <juan.zero@unitec.edu>
2020-10-14 10:11:06 -0600

    Add project files.

---
+++ HolaMundo/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <conio.h>
+
+int main() 
+{
+
+	//console output
+	/*Esta es un comentario
+	* en varias
+	* lineas*/
+
+	std::cout << "Bienvenidos a Programacion 3!\n";
+
+	_getch();
+	return 0;
+}
